cb909e92154f3e888ab7e5eb18718edddbc6ed61
Imre Bartis <imbartis@gmail.com>
2024-11-08 17:34:27 +0200

    implement linting workflow

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { setNotification } from "../reducers/notificationReducer";
